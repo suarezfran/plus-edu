c792979287493923a0e22e55903febe33ac1521a
Francisco Suarez <franciscoysuarez@hotmail.com>
2024-10-20 23:24:07 -0300

    change base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Fran313.github.io',
-  base: 'plus-edu'
+  site: 'https://Fran313.github.io'
 });
